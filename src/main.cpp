69579cc98af25ecce125964344fd7f06c28545ae
antonio-ramadas <up201303568@gcloud.fe.up.pt>
2018-04-21 15:35:26 +0100

    Add cli

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+#include <stdio.h>
 #include <utility>
 #include <vector>
 #include <set>
@@ -12,13 +14,39 @@ typedef vector<vector<int> > vecint2d;
 #include "FloodFill.h"
 #include "SmartFill.h"
 
-int main() {
-    vecint2d grid = Parser(0).getGrid();
+int main(int argc, char* argv[]) {
+    int gridOpt = 0, solver = 0;
 
-    //vector<set<pair<uint, uint> > > ff = FloodFill(grid).solve();
-    vector<set<pair<uint, uint> > > sf = SmartFill(grid).solve();
+    if (argc != 1 && argc != 3) {
+        cerr << "Usage: " << argv[0] << " <GRID_OPT> <SOLVER_OPT>" << endl;
+        cerr << "\t 0 <= GRID_OPT <= 4" << endl;
+        cerr << "\t 0 <= SOLVER_OPT <= 2" << endl;
+        return 1;
+    }
 
-    Printer::printGroups(sf);
+    if (argc == 1) {
+        cout << "Initialized default values:" << endl;
+        cout << "\t GRID_OPT = " << gridOpt << endl;
+        cout << "\t SOLVER_OPT = " << solver << endl << endl;
+    } else {
+        sscanf(argv[1], "%d", &gridOpt);
+        sscanf(argv[2], "%d", &solver);
+    }
+
+    vecint2d grid = Parser(gridOpt).getGrid();
+
+    vector<set<pair<uint, uint> > > groups;
+
+    switch (solver) {
+        case 0:
+            groups = FloodFill(grid).solve();
+            break;
+        default: //case 1:
+            groups = SmartFill(grid).solve();
+            break;
+    }
+
+    Printer::printGroups(groups);
     
     return 0;
 }
